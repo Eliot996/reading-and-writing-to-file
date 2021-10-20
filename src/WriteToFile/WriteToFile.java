7ac52d5d480f49dab032a10535215eee40f6de5d
Eliot996 <math10t6@stud.kea.dk>
2021-10-20 13:47:11 +0200

    added the two tasks from write to file exercise

--- src/WriteToFile/WriteToFile.java
+++ src/WriteToFile/WriteToFile.java
@@ -4,21 +4,68 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 public class WriteToFile {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("data/output.txt");
+    public static void main(String[] args) {
 
-        PrintStream printStream = new PrintStream(new FileOutputStream(file, true));
+        //guessNumber();
 
-        for (int j = 0; j < 100; j++) {
-            for (int i = 1; i <= 5; i++) {
-                printStream.append(Integer.toString(i));
-            }
-            printStream.println();
+        writeToFile();
+
+    }
+
+    public static void guessNumber(){
+        // generate a random number between 1-5
+        int number = (int) Math.ceil(Math.random()*5);
+
+        Scanner input = new Scanner(System.in);
+        File file = new File("data/guesses.txt");
+
+        // get guess and increment the counter, if it is not the same as the number go again
+        int guess;
+        int guessCount = 0;
+        do {
+            System.out.println("Guess the number between 1 - 5");
+            guess = input.nextInt();
+            guessCount++;
+        }while(guess != number);
+
+        // print the number and number of tries
+        System.out.println("Yes, the number was " + number);
+        System.out.println("You used " + guessCount + " tries");
+
+        try {
+            // make printStream and append to what is in the file
+            PrintStream printStream = new PrintStream(new FileOutputStream(file, true));
+            printStream.append(Integer.toString(guessCount)).append("\n");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
         }
-        printStream.close();
+    }
+
+    public static void writeToFile(){
+        Scanner input = new Scanner(System.in);
+
+        // get the desired file name, and make or get the file
+        System.out.print("What should the file be called? ");
+        String fileName = input.nextLine();
+        File file = new File("data/" + fileName + ".txt");
+
+        // get text to put into the file
+        System.out.println("What text do you want to save in the file? ");
+        String stringToSave = input.nextLine();
+
+        try {
+            // print to file
+            PrintStream printStream = new PrintStream(file);
+            printStream.print(stringToSave);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
 }
